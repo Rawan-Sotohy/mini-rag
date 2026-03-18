ed60d9d3c5cbc3ca405342d6d1adefc74c84ffe7
Rawan Sotohy <rawansotohy123@gmail.com>
2026-03-19 00:06:45 +0200

    chore: update main.py to include router and configure host/port

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv(".env")
+from routes import base
+
 app=FastAPI()
+app.include_router(base.base_router)
 
 """
 To run for development not production (alow for all 0.0.0.0) :
@@ -7,9 +12,11 @@ uvicorn main:app --reload --host 0.0.0.0 --port 8080
 URL ==> http://localhost:8080/welcome
 """
 
+"""
 @app.get("/welcome")
 def welcome():
     return {
     "message": "Hello World!"
     }
+    """
 
